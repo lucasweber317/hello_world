33cd50929c0dbdc68a0be54594dd139823e83121
lucasweber317 <lucasweber317@gmail.com>
2016-10-13 10:47:12 +0300

    It's all good, man!

---
+++ GitHub_Hello_World.py
@@ -0,0 +1,11 @@
+# This is simply a Hello_World to get things going on Windows / GitHub
+
+name = ("Lucas")
+
+age = 33
+
+location = ("Espoo")
+
+output = ("Hello world! My name is {}, I am {} years old and I live in the city of {}").format(name, age, location)
+
+print(output)
